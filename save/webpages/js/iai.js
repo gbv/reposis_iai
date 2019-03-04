5a4e4c9b1d88a17287041eecb305870695a2ea73
Kathleen Neumann <neumann@gbv.de>
2019-03-04 14:05:17 +0100

    initial commit

---
+++ save/webpages/js/iai.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+  // spam protection for mails
+  $('span.madress').each(function(i) {
+      var text = $(this).text();
+      var address = text.replace(" [at] ", "@");
+      $(this).after('<a href="mailto:'+address+'">'+ address +'</a>')
+      $(this).remove();
+  });
+
+  // activate empty search on start page
+  $("#mhh-searchMainPage").submit(function (evt) {
+    $(this).find(":input").filter(function () {
+          return !this.value;
+      }).attr("disabled", true);
+    return true;
+  });
+});
